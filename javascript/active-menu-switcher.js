be8e3805fddfbdcf3f45e3e3858df00ded9a6c86
Hafidz <gamerhacking@gmail.com>
2016-07-07 12:07:56 +0700

    Update active-menu-switcher.js
    Add comment.

--- javascript/active-menu-switcher.js
+++ javascript/active-menu-switcher.js
@@ -1,20 +1,26 @@
-// Menu active switcher
+/** Active Menu Item Switcher
+ * 
+ * Goal: Activate menu item which have id match with last url segment. 
+ */
+
+// Initialize the current url and path
 window.onload=function() {
 	var curUri = window.location.href;
 	var pathUri = window.location.pathname;
 
-	console.log("path uri: "+pathUri);
+	console.log("path uri: "+pathUri); // Checking got the pathname
 	var path = pathUri.split("/");
 
-	console.log("last uri: "+path[path.length-1]);
+	console.log("last uri: "+path[path.length-1]); // Checking got the last url segment
 	setAtcive(path[path.length-1]);
 }
 
+// Activate menu item by last url segment
 function setAtcive(id) {
-	var target = document.getElementById(id);
+	var target = document.getElementById(id); // Filter menu item id that match with last url segment
 	if (target !== null) {
-		target.classList.add("active");
+		target.classList.add("active"); // Activate matched menu item
 	} else {
-		document.getElementById("beranda").classList.add("active");
+		document.getElementById("home").classList.add("active"); // Else, activate "Home" menu
 	}
 }
